acc4bc3e50f8688d0b2a7b09184cf946f1c14ff4
DannaGA66 <140719302+DannaGA66@users.noreply.github.com>
2023-08-17 08:52:02 -0500

    Add files via upload

--- ProyectoFinal/js/app.js
+++ ProyectoFinal/js/app.js
@@ -5,5 +5,22 @@ const headerLista = document.getElementsByClassName("menu");
 const capturaClick = () => {
     window.location.href = 'https://www.google.com';
 }
+const capturaClick2 = () => {
+    window.location.href = 'https://www.google.com';
+}
+
+const seccion2 = document.createElement('section');
+const div1 = document.createElement('div');
+const btnGuardar = document.createElement('button');
+
+
+btnGuardar.id = "btnGuardar";
+seccion2.id = "sec2";
+div1.id = "div1";
+
+
+seccion2.appendChild(btnGuardar);
+document.body.appendChild(seccion2);
+
 
 
